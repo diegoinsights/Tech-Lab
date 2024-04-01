dc3a8d3d5e01db2f36df3e6023827a412777eb2c
diego <diegoinsights@gmail.com>
2024-04-01 04:36:52 -0300

    11 & 12 - sending request and getting the response

--- Scrapy_Masterclass/ebook_scraper/ebook_scraper/spiders/ebook.py
+++ Scrapy_Masterclass/ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -3,5 +3,8 @@ import scrapy
 
 class EbookSpider(scrapy.Spider):
     name = "ebook"
+    start_urls = ["https://books.toscrape.com/"]
 
-    
+    def parse(self, response):
+        print("[ OUR RESPONSE ]")
+        print(response)
