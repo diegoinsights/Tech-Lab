eacdf7f09dc8e45d4f0e04c7ba673c479b86e294
diego <diegoinsights@gmail.com>
2024-04-27 11:16:27 -0300

    staticmethod

--- Python_Masterclass/45_web_crawling/nightcrawler/spider.py
+++ Python_Masterclass/45_web_crawling/nightcrawler/spider.py
@@ -1,8 +1,7 @@
 from urllib.request import urlopen
-
 from link_seeker import LinkSeeker
-
 from sample import *
+from core import *
 
 
 class Spider:
@@ -10,10 +9,38 @@ class Spider:
     main_url = ''
     domain_name = ''
     lineup_file = ''
-    tracked_files = ''
+    tracked_file = ''
 
     lineup = set()
     tracked = set()
 
-    def __init__(self):
+    def __init__(self, project, main_url, domain_name):
+        Spider.project=project
+        Spider.main_url=main_url
+        Spider.domain_name=domain_name
+        Spider.lineup_file=Spider.project + '/lineup.txt'
+        Spider.tracked_file=Spider.project + '/tracked.txt'
+
+        self.boot()
+        self.crawl_page('First spider', Spider.main_url)
+
+    @staticmethod
+    def boot():
+        repo_project_creator(Spider.project)
+        data_files_creator(Spider.project, Spider.main_url)
+        Spider.lineup=to_collection(Spider.lineup_file)
+        Spider.tracked=to_collection(Spider.tracked_file)
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.tracked:
+            print(thread_name + 'Now crawling ' + page_url)
+            print('Lineup' + str(len(Spider.lineup)) + ' | Crawled ' + str(len(Spider.tracked)))
+            Spider.add_links_to_lineup(Spider.gather_links(page_url))
+            Spider.lineup.remove(page_url)
+            Spider.tracked.add(page_url)
+            Spider.update_files()
+
+
+
         
