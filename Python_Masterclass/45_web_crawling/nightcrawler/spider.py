a3fa3dcb6399daef43d3bf71547c4fb0ccf6994d
diego <diegoinsights@gmail.com>
2024-04-27 15:31:45 -0300

    webcrawler ok

--- Python_Masterclass/45_web_crawling/nightcrawler/spider.py
+++ Python_Masterclass/45_web_crawling/nightcrawler/spider.py
@@ -1,46 +1,70 @@
 from urllib.request import urlopen
-from link_seeker import LinkSeeker
-from sample import *
-from core import *
+
+from demo import *
+from domain import *
+from link_finder import LinkFinder
 
 
 class Spider:
-    project = ''
-    main_url = ''
+    project_name = ''
+    base_url = ''
     domain_name = ''
-    lineup_file = ''
-    tracked_file = ''
-
-    lineup = set()
-    tracked = set()
-
-    def __init__(self, project, main_url, domain_name):
-        Spider.project=project
-        Spider.main_url=main_url
-        Spider.domain_name=domain_name
-        Spider.lineup_file=Spider.project + '/lineup.txt'
-        Spider.tracked_file=Spider.project + '/tracked.txt'
-
+    queue_file = ''
+    crawled_file = ''
+    queue = set()
+    crawled = set()
+ 
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
         self.boot()
-        self.crawl_page('First spider', Spider.main_url)
-
+        self.crawl_page('First spider',Spider.base_url)
+ 
     @staticmethod
     def boot():
-        repo_project_creator(Spider.project)
-        data_files_creator(Spider.project, Spider.main_url)
-        Spider.lineup=to_collection(Spider.lineup_file)
-        Spider.tracked=to_collection(Spider.tracked_file)
-
+        create_project_dir(Spider.project_name)
+        create_data_files(Spider.project_name,Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
+ 
     @staticmethod
-    def crawl_page(thread_name, page_url):
-        if page_url not in Spider.tracked:
-            print(thread_name + 'Now crawling ' + page_url)
-            print('Lineup' + str(len(Spider.lineup)) + ' | Crawled ' + str(len(Spider.tracked)))
-            Spider.add_links_to_lineup(Spider.gather_links(page_url))
-            Spider.lineup.remove(page_url)
-            Spider.tracked.add(page_url)
+    def crawl_page(thread_name,page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + 'Now crawling ' +page_url)
+            print('Queue' + str(len(Spider.queue)) + ' | Crawled ' +str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_links(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
             Spider.update_files()
-
-
-
-        
+ 
+    @staticmethod
+    def gather_links(page_url):
+        html_string = ''
+        try:
+            response = urlopen(page_url)
+            if 'text/html' in response.getheader('Content-Type'):
+                html_bytes = response.read()
+                html_string = html_bytes.decode("utf-8")
+            finder = LinkFinder(Spider.base_url,page_url)
+            finder.feed(html_string)
+        except Exception as e:
+            print(str(e))
+            return set()
+        return finder.page_links()
+ 
+    @staticmethod
+    def add_links_to_queue(links):
+        for url in links:
+            if (url in Spider.queue) or (url in Spider.crawled):
+                continue
+            if Spider.domain_name != get_domain_name(url):
+                continue
+            Spider.queue.add(url)
+ 
+    @staticmethod
+    def update_files():
+        set_to_file(Spider.queue,Spider.queue_file)
+        set_to_file(Spider.crawled,Spider.crawled_file)
