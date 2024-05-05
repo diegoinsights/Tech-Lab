eafae2f7d61a3e1070227cba8d7015c4a6f3db27
diego <diegoinsights@gmail.com>
2024-05-05 12:04:20 -0300

    seleniumbase up

--- Master_Selenium_Webdriver_with_Python_using_SeleniumBase/test_home.py
+++ Master_Selenium_Webdriver_with_Python_using_SeleniumBase/test_home.py
@@ -11,3 +11,18 @@ class TestHomePage(BaseCase):
         self.assert_title_contains('SDET Unicorns')
 
 # para rodar o teste , basta digitar 'pytest' no terminal
+
+    def test_search_flow(self):
+        self.open('https://practice-react.sdetunicorns.com/')
+
+        # click on the search input field
+        self.click('.search-active')
+
+        # type lenovo in the search input field
+        self.type(selector="[placeholder='Search']", text='Lenovo')
+
+        # click on search button
+        self.click('.button-search')
+
+        # assert the text is visible
+        self.assert_text_visible('Showing Results for Lenovo')
